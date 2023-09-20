0937f03637a06e54aa6f19c23bc9c5e713d81106
Arthur <r.thur.light@gmail.com>
2023-09-20 15:00:41 +0000

    Changes

--- resources/views/emails/send-accepted-reserved.blade.php
+++ resources/views/emails/send-accepted-reserved.blade.php
@@ -1,6 +1,6 @@
 @component('mail::message')
     <p>Bonjour,</p>
-    <p>Votre réservation est enregistrée.</p>
+    <p>Votre réservation est confirmée.</p>
     <br>
     <p><b>Cadeau</b> : {{ $box->name }}</p>
     <p><b>Date réservée</b> : {{ $reservation }}</p>
